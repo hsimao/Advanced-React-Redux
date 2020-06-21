5321f20fd918021649dddd44bba4b8fc2cc608eb
hsimao <e087754958@gmail.com>
2020-06-21 17:47:09 +0800

    feat: 使用 jest beforeEach 鉤子封裝常用邏輯

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,14 +4,16 @@ import App from 'components/App'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
-it('Shows a comment box', () => {
-  const wrapper = shallow(<App />)
+let wrapper
 
+beforeEach(() => {
+  wrapper = shallow(<App />)
+})
+
+it('Shows a comment box', () => {
   expect(wrapper.find(CommentBox).length).toEqual(1)
 })
 
 it('Shows a comment list', () => {
-  const wrapper = shallow(<App />)
-
   expect(wrapper.find(CommentList).length).toEqual(1)
 })
