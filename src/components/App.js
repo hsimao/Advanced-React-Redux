5b86846354993df9c5b2cc8e545d646ba0ea4540
hsimao <e087754958@gmail.com>
2020-06-27 16:23:57 +0800

    feat: add react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
 export default () => {
   return (
     <div>
-      <CommentBox />
-      <CommentList />
+      <Route path='/' exact component={CommentList}></Route>
+      <Route path='/post' component={CommentBox}></Route>
     </div>
   )
 }
