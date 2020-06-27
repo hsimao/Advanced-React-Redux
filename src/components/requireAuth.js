236e265174d1deceba68a88e0e80600582c84bb1
hsimao <e087754958@gmail.com>
2020-06-27 17:14:08 +0800

    feat: requireAuth HOC component

---
+++ src/components/requireAuth.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export default ChildComponent => {
+  class ComposedComponent extends Component {
+    componentDidMount() {
+      this.shouldNavigateSway()
+    }
+
+    componentDidUpdate() {
+      this.shouldNavigateSway()
+    }
+
+    shouldNavigateSway() {
+      if (!this.props.auth) {
+        this.props.history.push('/')
+      }
+    }
+
+    render() {
+      return <ChildComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps({ auth }) {
+    return { auth }
+  }
+
+  return connect(mapStateToProps)(ComposedComponent)
+}
